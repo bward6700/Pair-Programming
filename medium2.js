8d260b94bd356c18f5345958ebdfb5b0d9d7b000
Madison Hill <madisonhill@macbook-pro.attlocal.net>
2022-05-03 13:54:29 -0400

    probFixed

--- medium2.js
+++ medium2.js
@@ -1,5 +1,4 @@
 // Create a function that accepts two strings, then determines whether or not the first string is an anagram of the
-// second string by returning a boolean.
 
 function Compare(anaOne, anaTwo){
     if (anaOne.length !== anaTwo.length) {
@@ -8,6 +7,8 @@ function Compare(anaOne, anaTwo){
   
    return anaOne.toLowerCase().split("").sort().join("") === anaTwo.toLowerCase().split("").sort().join("")
   }
-  
-  console.log(Compare("Friend", "Finder")) ;//true
+ 
+
+
+  console.log(Compare("Bruce Springsteen ","Creep brings tunes")) ;//true
   console.log(Compare("Dark", "Rock"))//false
